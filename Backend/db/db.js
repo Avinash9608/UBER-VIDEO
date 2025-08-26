2d58160197e6e65f56552d853e2627561dbc46cc
Avinash Kumar <avinashmadhukar4@gmail.com>
2025-08-26 13:31:26 +0530

    User Register

---
+++ Backend/db/db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+function connectToDb() {
+  mongoose
+    .connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+      console.log("Error connecting to MongoDB", err);
+    });
+}
+
+module.exports = connectToDb;
